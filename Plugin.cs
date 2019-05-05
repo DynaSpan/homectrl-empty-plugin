0ca779341645b376a9bab6d7b930914a1c37a0d4
Milan <m.drossaerts@gmail.com>
2019-05-05 20:39:10 +0200

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HomeCTRL.Plugin.EmptyExample
+{
+    public class Plugin : IPlugin
+    {
+        public async Task<bool> Startup()
+        {
+            Console.WriteLine("TESSSTTTTT");
+
+            return await Task.FromResult<bool>(true);
+        }
+    }
+}
